4316c019ca0b56f99740411bb7c95cb783c9a659
ThienNhanTranConnan <tran-cot@3il.fr>
2020-06-07 18:26:02 +0200

    Déposition de fichiers images terminées

--- src/projet/view/benevole/ModelBenevole.java
+++ src/projet/view/benevole/ModelBenevole.java
@@ -1,14 +1,27 @@
 package projet.view.benevole;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import jfox.commun.exception.ExceptionValidation;
 import jfox.javafx.util.UtilFX;
 import projet.commun.IMapper;
 import projet.dao.DaoBenevole;
+import projet.dao.DaoPersonne;
 import projet.data.Benevole;
+import projet.data.Participant;
 import projet.data.Utilisateur;
+import projet.view.systeme.ModelConfig;
+import projet.view.systeme.ModelConfigPermis;
 
 public class ModelBenevole {
 
@@ -16,18 +29,25 @@ public class ModelBenevole {
 
 	private final ObservableList<Benevole> liste = FXCollections.observableArrayList();
 
-	private final Benevole courant = (Benevole)Utilisateur.getCourant().getUtilisateur();
-
+	private final Benevole courant = (Benevole) Utilisateur.getCourant().getUtilisateur();
+	private final Property<Image> permis = new SimpleObjectProperty<>();
 	// Autres champs
 	@Inject
 	private IMapper mapper;
 	@Inject
 	private DaoBenevole daoBenevole;
-
+	@Inject
+	private DaoPersonne daoPersonne;
+	@Inject
+	private ModelConfigPermis modelConfigPermis;
 	// Getters
 
 	public Benevole getCourant() {
 		return courant;
+	}
+
+	public Property<Image> permisProperty() {
+		return permis;
 	}
 
 	// Actualisations
@@ -36,13 +56,24 @@ public class ModelBenevole {
 
 	public void preparerAjouter() {
 		mapper.update(courant, new Benevole());
+		permis.setValue(null);
 	}
 
 	public void preparerModifier(Benevole item) {
 		mapper.update(courant, daoBenevole.retrouver(item.getId()));
 	}
 
-	public void validerMiseAJour() {
+	public void preparerModifierImagePermis(Benevole item) {
+		File fichier = getFichierSchemaCourantPermis();
+		if (fichier.exists()) {
+			permis.setValue(new Image(fichier.toURI().toString()));
+		} else {
+			permis.setValue(null);
+		}
+
+	}
+
+	public void sauvegarder() {
 
 		// Vérifie la validité des données
 
@@ -51,22 +82,65 @@ public class ModelBenevole {
 		if (courant.getNom() == null || courant.getNom().isEmpty()) {
 			message.append("\nLe nom ne doit pas être vide.");
 		} else if (courant.getNom().length() > 50) {
-			message.append("\nLe nom est trop long : 50 maxi.");
+			message.append("\nLe nom est trop long : 50 caractères maxi.");
 		}
+		if (courant.getPrenom() == null || courant.getPrenom().isEmpty()) {
+			message.append("\nLe prénom ne doit pas être vide.");
+		} else if (courant.getPrenom().length() > 50) {
+			message.append("\nLe prénom est trop long : 50 caractères maxi.");
+		}
+		if (courant.getTel() == null || courant.getTel().isEmpty()) {
+			message.append("\nLe numéro de téléphone ne doit pas être vide.");
+		} else if (courant.getTel().length() != 10) {
+			message.append("\nLe numéro de téléphone doit contenir exactement 10 chiffres.");
+		} else if (!courant.getTel().matches("[1234567890]{10}")) {
+			message.append("\nLe numéro de téléphone est incorrect.");
+			message.append("\nVeuillez entrer que des chiffres et sans espace.");
+		}
+		if (courant.getMail() == null || courant.getMail().isEmpty()) {
+			message.append("\nL'adresse e-mail ne doit pas être vide.");
+		} else if (courant.getMail().length() > 50) {
+			message.append("\nL'adresse e-mail est trop longue : 50 maxi.");
+		}
+		if (courant.getAdresse() == null || courant.getAdresse().isEmpty()) {
+			message.append("\nL'adresse ne doit pas être vide.");
+		} else if (courant.getAdresse().length() > 50) {
+			message.append("\nL'adresse est trop longue : 50 maxi.");
+		}
+		if (message.length() > 0) {
+			throw new ExceptionValidation(message.toString().substring(1));
+		}
+
 		// Effectue la mise à jour
 
 		if (courant.getId() == null) {
+			// Insertion
+			courant.setId(daoPersonne.inserer(courant));
+		} else {
 			// modficiation
-			daoBenevole.modifier(courant);
+			daoPersonne.modifier(courant);
+		}
 
+	}
+
+	public void sauvegarderImagePermis() {
+		if (permis.getValue() == null) {
+			getFichierSchemaCourantPermis().delete();
+		} else {
+			try {
+				ImageIO.write(SwingFXUtils.fromFXImage(permis.getValue(), null), "JPG",
+						getFichierSchemaCourantPermis());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
-	public void supprimer(Benevole item) {
-
-		daoBenevole.supprimer(item.getId());
-		System.out.println(UtilFX.findNext(liste, item));
-		mapper.update(courant, UtilFX.findNext(liste, item));
+	// Méthodes auxiliaires
+	public File getFichierSchemaCourantPermis() {
+		String nomFichier = String.format("%06d.jpg", courant.getId());
+		File dossierSchemas = modelConfigPermis.getDossierSchemas();
+		return new File(dossierSchemas, nomFichier);
 	}
 
 }
